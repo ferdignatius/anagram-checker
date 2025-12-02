da3f9533d84f90584d7b5ff6008bfbf0311542bf
Ferdinand Ignatius Surya <84720954+ferdignatius@users.noreply.github.com>
2025-12-02 20:32:24 +0700

    fix isAlpha bugs

--- logicIsAnagram/isAlpha.js
+++ logicIsAnagram/isAlpha.js
@@ -1,7 +1,7 @@
 export default function isAlpha(str) {
   for (let i = 0; i < str.length; i++) {
     const code = str.charCodeAt(i);
-    if(code < 97 && code > 122) return false;
+    if(code < 97 || code > 122) return false;
   }
   return true;
 }
